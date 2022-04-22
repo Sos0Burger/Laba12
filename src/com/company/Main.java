dcc22402aff77f25e3db9f8b10075af148e2f78a
SosoBurger <stulnikovan1337@gmail.com>
2022-04-22 21:57:52 +0300

    Наканецта все ip записываются

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
-import java.io.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,17 +12,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите текст");
+
         String ip =in.nextLine();
-        Pattern pattern = Pattern.compile("(\\s|^)(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
+        boolean isFind;
+        int matches = 0;
+
+        Pattern pattern = Pattern.compile("((^|\\s)(([1-9]?\\d|1\\d\\d|2[0-5][0-5]|2[0-4]\\d)\\.){3}([1-9]?\\d|1\\d\\d|2[0-5][0-5]|2[0-4]\\d))");
         Matcher m = pattern.matcher(ip);
-        try{
-            FileWriter f = new FileWriter("ips.txt");
-            while (m.find()){
-                System.out.println(m.group());
-                //f.write(m.group() + "\n");
-                System.out.println("IP записан");
-                f.flush();
-            }
+        try( FileWriter f = new FileWriter("ips.txt")){
+            do{
+                isFind = m.find();
+                if (isFind) {
+                    f.write(m.group().replace(" ", "") + "\n");
+                    System.out.println("IP записан");
+                    f.flush();
+                    matches++;
+                }
+                if(!isFind & matches == 0) {
+                    System.out.println("Ip адресов не найдено");
+                    f.write("IP адресов не найдено :_(");
+                }
+            }while (isFind);
         }
         catch (IOException ex){
             System.out.println(ex.getMessage());
